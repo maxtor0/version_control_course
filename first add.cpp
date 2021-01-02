cf072d9c3a99c0e722d2c1d94997ce364b548da5
John George <Sci.JohnGeorge.2017@alexu.edu.eg>
2021-01-02 09:56:24 +0200

    Adding sum to 'fist add.cpp'

--- first add.cpp
+++ first add.cpp
@@ -1,6 +1,13 @@
 #include<iostream>
+
 using namespace std;
+
+int sum(int x, int y){
+	return x+y;
+}
+
 int main(){
-    cout<<"First add";
-    return 0;
+	cout<<"First add";
+	cout<<sum(1+3)<<endl;
+	return 0;
 }
