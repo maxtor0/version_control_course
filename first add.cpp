b6be679d67927dc8b074d924395e1fca6b6a9e36
Abdelrahman Yasser <maxtor11.aa@gmail.com>
2021-01-02 12:15:12 +0200

    added divide function

--- first add.cpp
+++ first add.cpp
@@ -8,10 +8,13 @@ int sum(int x, int y){
 int sub(int x, int y){
 	return x-y;
 }
-
+int div(int x, int y){
+	return x/y;
+}
 int main(){
 	cout<<"First add";
 	cout<<sum(1,3)<<endl;
 	cout<<sub(4,1)<<endl;
+	cout<<div(4,2)<<endl;
 	return 0;
 }
