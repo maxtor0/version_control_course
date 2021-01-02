f6b5a64647c6f978e2fc88859c3ed83723352d59
Abdelrahman Yasser <maxtor11.aa@gmail.com>
2021-01-02 10:11:48 +0200

    Added sub function and fixed add function in 'first add.cpp'

--- first add.cpp
+++ first add.cpp
@@ -5,9 +5,13 @@ using namespace std;
 int sum(int x, int y){
 	return x+y;
 }
+int sub(int x, int y){
+	return x-y;
+}
 
 int main(){
 	cout<<"First add";
-	cout<<sum(1+3)<<endl;
+	cout<<sum(1,3)<<endl;
+	cout<<sub(4,1)<<endl;
 	return 0;
 }
